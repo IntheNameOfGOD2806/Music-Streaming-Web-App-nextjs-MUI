1bc60b2ab79f8c92eacfa5ed06faf2015bbf4691
Trần thành Đạt <101255344+IntheNameOfGOD2806@users.noreply.github.com>
2024-01-22 23:14:35 +0700

    cmit

--- src/components/Music/WaveTrack.tsx
+++ src/components/Music/WaveTrack.tsx
@@ -1,51 +1,39 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 // import Timeline from "wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js";
 import { useWavesurfer } from "@wavesurfer/react";
 import { useCallback } from "react";
-
+import { WaveSurferOptions } from "wavesurfer.js";
 const WaveTrack = () => {
   const searchParams = useSearchParams();
   const audio = searchParams.get("audio");
   console.log(audio);
-  //////////////
-  const containerRef = useRef(null);
- 
-  const { wavesurfer, isPlaying, currentTime } = useWavesurfer({
-    container: containerRef,
-    height: 100,
-    waveColor: "rgb(200, 0, 200)",
-    progressColor: "rgb(100, 0, 100)",
-    url: `${process.env.NEXT_PUBLIC_NEXTFRONTEND_URL}api?audio=${audio}`,
-  });
+  //
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const optionMemo = useMemo(() => {
+    return {
+      container: containerRef,
+      height: 100,
+      waveColor: "rgb(200, 0, 200)",
+      progressColor: "rgb(100, 0, 100)",
+      url: `${process.env.NEXT_PUBLIC_NEXTFRONTEND_URL}api?audio=${audio}`,
+    };
+  }, []);
+  const { wavesurfer, isPlaying, currentTime } = useWavesurfer(optionMemo);
+  //
   const onPlayPause = useCallback(() => {
     wavesurfer && wavesurfer.playPause();
   }, [wavesurfer]);
+  //
   useEffect(() => {
+    if (!containerRef.current) return;
+    console.log("render wave track");
     return () => {
       wavesurfer && wavesurfer.destroy();
     };
-  }, [containerRef]);
-
-  ////////////////
-
-  const wavetrackRef = useRef<HTMLDivElement>(null);
-  // useEffect(() => {
-  //   const element =  wavetrackRef.current;
-  //   if (element) {
-  //     const wavesurfer = WaveSurfer.create({
-  //       container: element,
-  //       waveColor: "rgb(200, 0, 200)",
-  //       progressColor: "rgb(100, 0, 100)",
-  //       url: `${process.env.NEXT_PUBLIC_NEXTFRONTEND_URL}api?audio=${audio}`,
-  //     });
-  //     wavesurfer.on("click", () => {
-  //       wavesurfer.play();
-  //     });
-  //   }
-  // }, []);
-
+  }, []);
   return (
     <>
       <div ref={containerRef} className="wave-track"></div>{" "}
@@ -53,5 +41,4 @@ const WaveTrack = () => {
     </>
   );
 };
-
 export default WaveTrack;
